ff883c3cb09d1e8a461ba6c367d6442808bc2a50
Beatriz <beatrizschulter@gmail.com>
2022-04-07 17:17:05 -0300

    aplicando tratamento de exceções

--- Car rental/src/application/Program.java
+++ Car rental/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,37 +14,50 @@ import model.services.BrazilTaxService;
 import model.services.RentalService;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
-    	
-    	Locale.setDefault(Locale.US); //para escolher virgula ou ponto
+	public static void main(String[] args) throws Exception {
 
-    	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm");
-    	
-    	Scanner in = new Scanner(System.in);
-    	
-    	
-    	System.out.println("Enter car rental data");
-    	System.out.print("Enter car model: ");
-    	String carModel = in.nextLine();
-    	System.out.print("pickup time: ");
-    	Date pickup = dateFormat.parse(in.nextLine());
-    	System.out.print("return time: ");
-    	Date returns = dateFormat.parse(in.nextLine());
-    	
-    	
-    	CarRental rental1 = new CarRental(pickup, returns, new Vehicle (carModel));
-    	
-    	System.out.print("Enter price per hour: ");
-    	Double priceHour = in.nextDouble();
-    	System.out.print("Enter price per day: ");
-    	Double priceDay = in.nextDouble();
-    	
-    	System.out.println("INVOICE:");
-    	
-    	RentalService rentalService1 = new RentalService(priceHour, priceDay, new BrazilTaxService());
-    	
-    	rentalService1.processInvoice(rental1);
-    	System.out.println(rental1);
+		Locale.setDefault(Locale.US); // para escolher virgula ou ponto
 
-    }
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm");
+
+		Scanner in = null;
+		
+		String carModel = null;
+		Double priceHour= null, priceDay = null;
+		Date pickup = null, returns = null;
+		
+		
+		try {			
+			
+			in = new Scanner(System.in);
+
+			System.out.println("Enter car rental data");
+			System.out.print("Enter car model: ");
+			carModel = in.nextLine();
+			System.out.print("pickup time: ");
+			pickup = dateFormat.parse(in.nextLine());
+			System.out.print("return time: ");
+			returns = dateFormat.parse(in.nextLine());
+	
+	
+			System.out.print("Enter price per hour: ");
+			priceHour = in.nextDouble();
+			System.out.print("Enter price per day: ");
+			priceDay = in.nextDouble();
+
+		}catch(InputMismatchException imex) {
+			System.err.println("Deu erro na entrada...");
+		}catch(ParseException pex) {
+			System.err.println("Data inválida ");
+		}
+		
+		System.out.println("INVOICE:");
+
+		CarRental rental1 = new CarRental(pickup, returns, new Vehicle(carModel));
+		RentalService rentalService1 = new RentalService(priceHour, priceDay, new BrazilTaxService());
+
+		rentalService1.processInvoice(rental1);
+		System.out.println(rental1);
+
+	}
 }
